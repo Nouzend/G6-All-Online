83d9e6cd57af082fc48f3293b773308c25bda66d
NineSupawit <114071230+NineSupawit@users.noreply.github.com>
2023-05-11 15:45:45 +0700

    Update index.jsx

--- src/Availability/components/Home/List/index.jsx
+++ src/Availability/components/Home/List/index.jsx
@@ -1,14 +1,32 @@
-//นาย
-import React from 'react';
+import React, { useState } from 'react';
 import ListItem from './ListItem';
 import './styles.css';
+import ShopCart from '../../../../Chachaphong/shops/ShopCart';
 
-const List = ({ list }) => (
-  <div className='list-wrap'>
-    {list.map((item) => (
-      <ListItem key={item.id} item={item} />
-    ))}
-  </div>
-);
+const List = ({ list }) => {
+  const [count, setCount] = useState(0);
+
+  const addToCart = (item) => {
+    ShopCart.addItem(item);
+  };
+
+
+  return (
+    <div className='list-item'>
+      <div className='list-wrap'>
+        {list.map((item) => (
+          <div key={item.id} className='item-container'>
+            <ListItem item={item} />
+            <div className='item-overlay'>
+              <button className='add-to-cart-button' onClick={() => addToCart(item)}>
+                <i className='fa-solid fa-plus' style={{ float: 'right' }}></i>
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default List;
