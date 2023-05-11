9eb74a5ee4d3279cefc600d15cb5ad20cc1565ec
NineSupawit <114071230+NineSupawit@users.noreply.github.com>
2023-05-11 15:40:37 +0700

    Update Home.jsx

--- src/Chachaphong/MainPage/Home.jsx
+++ src/Chachaphong/MainPage/Home.jsx
@@ -1,7 +1,5 @@
 import React from "react"
-import Categories from "./Categories"
 import "./Home.css"
-import SliderHome from "./Slider"
 import Navbar from "../../Metha/sup-compo/navbar/navbarMain"
 import Tipchumporn from "../../Availability/pages/Home"
 
@@ -13,8 +11,6 @@ const Home = ({t,i18n}) => {
         <Navbar t={t} i18n={i18n}/>
       <section className='home'>
         <div className='container d_flex'>
-          <Categories t={t} i18n={i18n} />
-          <SliderHome />
           <Tipchumporn/>
         </div>
       </section>
